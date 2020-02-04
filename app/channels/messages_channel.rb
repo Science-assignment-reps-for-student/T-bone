c799fb9d7df6687fc9fb2ef5aaeaa204c160b4ea
JeongWooYeong <wjd030811@gmail.com>
2020-02-04 15:04:56 +0900

    [FIX] fixed socket test

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,10 +1,14 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
     stream_from 'chat_message_channel'
-    ActionCable.server.broadcast('chat_message_channel', content: 'hello world!')
+    ActionCable.server.broadcast('chat_message_channel', message: 'hello world!')
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    ActionCable.server.broadcast('chat_message_channel', message: data)
   end
 end
