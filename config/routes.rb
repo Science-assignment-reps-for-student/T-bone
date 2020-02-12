e67f542ad81d012b03e9d616207385e769e37b79
JeongWooYeong <wjd030811@gmail.com>
2020-02-12 21:11:36 +0900

    [FIX] fixed routing error

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/single/:file_id', to: 'show_file#show_single'
   get '/multi/:file_id', to: 'show_file#show_multi'
   get '/excel/:file_id', to: 'show_file#show_excel'
-  get '/notice/:file_id', to: 'show_file#show_notice'
+  get '/notice/:homework_id', to: 'show_file#show_notice'
 
   # create files
   post '/single/:homework_id', to: 'create_file#create_single'
