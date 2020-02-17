d8b1800aea14a224b71a64a755b25e89f337c59b
JeongWooYeong <wjd030811@gmail.com>
2020-02-18 03:24:59 +0900

    [FIX] fixed redis url

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # show files
+    # show files
   get '/single/:file_id', to: 'show_file#show_single'
   get '/multi/:file_id', to: 'show_file#show_multi'
   get '/excel/:homework_id', to: 'show_file#show_excel'
   get '/notice/:file_id', to: 'show_file#show_notice'
   get '/file-zip/:homework_id', to: 'show_file#show_many'
 
-  # create files
+    # create files
   post '/single/:homework_id', to: 'create_file#create_single'
   post '/multi/:homework_id', to: 'create_file#create_multi'
   post '/excel/:homework_id', to: 'create_file#create_excel'
 
-  # update files
+    # update files
   put '/single/:homework_id', to: 'update_file#update_single'
   put '/multi/:homework_id', to: 'update_file#update_multi'
   put '/excel/:homework_id', to: 'update_file#update_excel'
 
-  # APIS
+    # APIS
   get '/homework/:homework_id', to: 'api#show'
   patch '/homework/:homework_id', to: 'api#update'
   delete '/homework/:homework_id', to: 'api#destroy'
