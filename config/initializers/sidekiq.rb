f00d1fde15c1c74077cb6187f7192e754d3f9761
JeongWooYeong <wjd030811@gmail.com>
2020-02-18 15:45:58 +0900

    [FIX] fixed config redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['SCARFS_DB']}:6379/1" }
+  config.redis = { url: "redis://#{ENV['REDIS_URL']}:6379/1" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['SCARFS_DB']}:6379/1" }
+  config.redis = { url: "redis://#{ENV['REDIS_URL']}:6379/1" }
 end
 
 Sidekiq.default_worker_options = { retry: 1 }
