f9e82d41d3086163c94a42122e96b79831046498
JeongWooYeong <wjd030811@gmail.com>
2020-02-18 16:55:06 +0900

    [RM] removed queue

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,5 @@ module TBone
     config.api_only = true
     config.time_zone = 'Seoul'
     config.active_record.default_timezone = :utc
-
-    config.action_mailer.deliver_later_queue_name = 'default_mailer_queue'
   end
 end
