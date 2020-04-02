f38c9625669b71b131d3be53e6a3f0d94959d15c
JeongWooYeong <wjd030811@gmail.com>
2020-04-02 18:58:15 +0900

    [RM] removed about board

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :single_files, dependent: :delete_all
-  has_many :chats, dependent: :delete_all
   has_many :self_evaluations, dependent: :delete_all
-  has_many :mutual_evaluations, dependent: :delete_all
+  has_many :mutual_evaluations, dependent: :delete_all\
 end
