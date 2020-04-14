75c04674f08dd3f1ec6124e661208253997404e3
JeongWooYeong <wjd030811@gmail.com>
2020-04-14 18:33:16 +0900

    [ADD] added db relation about board, comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :single_files, dependent: :delete_all
   has_many :self_evaluations, dependent: :delete_all
-  has_many :mutual_evaluations, dependent: :delete_all\
+  has_many :mutual_evaluations, dependent: :delete_all
+  has_many :boards, dependent: :delete_all
+  has_many :comments, dependent: :delete_all
 end
