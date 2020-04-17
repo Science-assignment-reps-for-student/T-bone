35adfdae92c7be4a9a914fcd948f9d2c4b0c0e4f
JeongWooYeong <wjd030811@gmail.com>
2020-04-18 00:09:09 +0900

    [FIX] fixed api addr to plain text

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,7 +3,7 @@ class ApiController < ApplicationController
 
   def email
     requires(:auth_code, :target)
-    return render status: :forbidden unless request.remote_ip == ENV['SCARFS_IP']
+    return render status: 403 unless request.remote_ip == '54.180.174.253'
 
     MailMailer.auth(params[:target], params[:auth_code]).deliver_later
   end
