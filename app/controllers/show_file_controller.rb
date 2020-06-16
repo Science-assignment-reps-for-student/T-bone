55fa3eb4138bd78f6202f130647c30293b8b2b91
JeongWooYeong <wjd030811@gmail.com>
2020-06-16 18:30:29 +0900

    [FIX] fixed zip command

--- app/controllers/show_file_controller.rb
+++ app/controllers/show_file_controller.rb
@@ -83,7 +83,7 @@ class ShowFileController < ApplicationController
     end
 
     FileUtils.rm_rf("#{path}/[#{homework_type}]#{homework.homework_title}.zip")
-    system("zip --junk-paths #{path}/'[#{homework_type}]#{homework.homework_title}'.zip #{path}/#{homework.id}/*")
+    system("zip -r -j #{path}/'[#{homework_type}]#{homework.homework_title}'.zip #{path}/#{homework.id}/*")
     send_file("#{path}/[#{homework_type}]#{homework.homework_title}.zip")
   end
 end
