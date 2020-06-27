ca0c90d38dd636ef4f4b3b84ca0ffae58a6daecc
JeongWooYeong <wjd030811@gmail.com>
2020-06-27 16:02:29 +0900

    [ADD] excel username

--- app/models/excel_file.rb
+++ app/models/excel_file.rb
@@ -16,7 +16,7 @@ class ExcelFile < ApplicationRecord
       class_number = team.users.last.user_number / 100 - 10
       team.users.order(user_number: :asc).each do |user|
         row = row_set[class_number - 1]
-        row_set[class_number - 1] += 2
+        row_set[class_number - 1] += 3
 
         sheets[class_number - 1].row(row)[0] = team.team_name
         sheets[class_number - 1].row(row)[1] = user.user_number
@@ -30,8 +30,12 @@ class ExcelFile < ApplicationRecord
                                                    target_id: user.id)
                                             .order(user_number: :asc)
 
-        sheets[class_number - 1].row(row)[5..8] = mutual_evaluation.map(&:communication)
-        sheets[class_number - 1].row(row + 1)[5..8] = mutual_evaluation.map(&:cooperation)
+        sheets[class_number - 1].row(row)[5..8] =
+          mutual_evaluation.map do |evaluation|
+            evaluation.user.user_name
+          end
+        sheets[class_number - 1].row(row + 1)[5..8] = mutual_evaluation.map(&:communication)
+        sheets[class_number - 1].row(row + 2)[5..8] = mutual_evaluation.map(&:cooperation)
         sheets[class_number - 1].row(row)[9] = mutual_evaluation.map(&:communication)
                                                                 .sum
         sheets[class_number - 1].row(row + 1)[9] = mutual_evaluation.map(&:cooperation)
@@ -72,10 +76,10 @@ class ExcelFile < ApplicationRecord
     sheets.each do |sheet|
       sheet.default_format = Spreadsheet::Format.new(horizontal_align: :center)
       sheet.merge_cells(0, 10, 0, 12)
-      (10..12).each { |i| sheet.merge_cells(1, i, 2, i) }
+      (10..12).each { |i| sheet.merge_cells(1, i, 2, i)}
       (0..12).each do |i|
         sheet.merge_cells(0, i, 2, i) unless (10..12).include?(i)
-        (2..22).each { |j| sheet.merge_cells(j * 2 - 1, i, j * 2, i) unless (4..9).include?(i) }
+        (1..22).each { |j| sheet.merge_cells(j * 3, i, j * 3 + 2, i) unless (4..9).include?(i)}
       end
 
       sheet.row(0).push('조',
@@ -92,8 +96,10 @@ class ExcelFile < ApplicationRecord
       sheet.row(1)[10] = '과학적 정확성'
       sheet.row(1)[11] = '의사소통'
       sheet.row(1)[12] = '흥미/태도(협력)'
-      (3..44).each do |i|
-        sheet.row(i)[4] = if i.odd?
+      (3..68).each do |i|
+        sheet.row(i)[4] = if (i % 3).zero?
+                            '평가자'
+                          elsif i % 3 == 1
                             '의사소통'
                           else
                             '공동체(협력)'
